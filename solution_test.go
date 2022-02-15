1d706c4209989c9b855e26db49d9b4deea5b182a
Irakli Razmadze <sysadmin@fina2.net>
2022-02-15 09:39:00 +0400

    feat: tests for solution

---
+++ solution_test.go
@@ -0,0 +1,20 @@
+package solution
+
+import (
+	"testing"
+
+	"fmt"
+
+	"github.com/kyokomi/emoji/v2"
+)
+
+func TestMessage(t *testing.T) {
+	expectedOutput := emoji.Sprint("Hello :world_map:!")
+
+	actual := getMessage()
+
+	if actual != expectedOutput {
+		errorMsg := fmt.Sprint("expected output to be - ", expectedOutput, " - but instead got - ", actual)
+		t.Errorf(errorMsg)
+	}
+}
